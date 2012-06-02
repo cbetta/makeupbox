0cd6d840ab657087796f09977088d82fc83c46ad
Cristiano Betta <cbetta@gmail.com>
2012-06-02 10:40:44 +0100

    Added a lot of boilerplate images

--- www/scripts/data.js
+++ www/scripts/data.js
@@ -1,20 +1,80 @@
 var appModel = {
   items: [
     {
-  		brand: "Chanel",
-  		category: "eye shadow",
-  		title: "Ombre Essentielle",
-  		color: "52 Beige",
-  		image: ko.observable("images/no-image.png"),
-  		rating: 3,
+    	brand: "Lancome",
+    	category: "eye liner",
+    	title: "Artliner",
+    	color: "01 Noir",
+    	image: ko.observable("images/products/lancome_artliner.png"),
+    	rating: 4
     },
     {
-      brand: "Chanel",
-      category: "lipstick",
-      title: "Rouge Coco",
-      color: "21 Rivoli",
-      image: ko.observable("images/no-image.png"),
-      rating: 4
+    	brand: "Dior",
+    	category: "eye shadow",
+    	title: "5 Couleurs",
+    	color: "Tropical Light",
+    	image: ko.observable("images/products/dior_tropicallight.png"),
+    	rating: 4,
+    	review: "Lovely colours, but a bit too shiny for during the day. Perfect for a going-out look though!",
+    	notes: "Combine with Urban Decay gold eyeliner"
+    },
+    {
+    	brand: "Illamasqua",
+    	category: "eye shadow",
+    	title: "Liquid Metal",
+    	color: "Resolute",
+    	image: ko.observable("images/products/illamasqua_resolute.png"),
+    	rating: 2
+    },
+    {
+    	brand: "Urban Decay",
+    	category: "eye shadow",
+    	title: "Palette",
+    	color: "Naked",
+    	image: ko.observable("images/products/urbandecay_gash.png"),
+    	rating: 4
+    },
+    {
+    	brand: "MAC",
+    	category: "mascara",
+    	title: "Zoom",
+    	color: "Black",
+    	image: ko.observable("images/products/mac_zoom.png"),
+    	rating: 4
+    }
+  ],
+
+  friends: [
+    {
+      name: "Sophia D.",
+      image: ko.observable('images/friends/sophia.png'),
+    },
+    {
+      name: "Isabella J.",
+      image: ko.observable('images/friends/isabella.png'),
+    },
+    {
+      name: "Emma K.",
+      image: ko.observable('images/friends/emma.png'),
+    },
+    {
+      name: "Ella V.",
+      image: ko.observable('images/friends/ella.png'),
+    },
+  ],
+
+  looks: [
+    {
+      name: "Over The Air 2012",
+      image: ko.observable('images/looks/ota2012.png'),
+    },
+    {
+      name: "Over The Air 2011",
+      image: ko.observable('images/looks/ota2011.png'),
+    },
+    {
+      name: "Wedding Trish",
+      image: ko.observable('images/looks/wedding.png'),
     }
   ],
 
@@ -23,6 +83,7 @@ var appModel = {
 
   selectedCategory: null,
   selectedItem: null,
+  selectedFriend: null,
 
   setSelectedCategory: function(cat) {
     this.selectedCategory = cat;
@@ -30,6 +91,10 @@ var appModel = {
 
   setSelectedItem: function(index) {
     this.selectedItem = index();
+  },
+
+  setSelectedFriend: function(index) {
+    this.selectedFriend = index();
   },
 
   iconFor: function(cat) {
@@ -41,10 +106,11 @@ var appModel = {
   },
 
   currentItem: function() {
-    if (this.selectedItem == null)
-      return {title: null, category: null, image: null, color: null, brand: null, rating: 3 };
-    else
-      return this.items[this.selectedItem];
+    if (this.selectedItem == null) {
+      this.items.push ({ title: null, category: null, image: ko.observable("images/no-image.png"), color: null, brand: null, rating: 3 });
+      this.selectedItem = this.items.length-1;
+    }
+    return this.items[this.selectedItem];
   },
 
   setImage: function(data_url) {
